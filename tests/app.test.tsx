cea4f534a7879d151967196da306381dd88a383a
Irakli Kverenchkhiladze <irakli_kverenchkhiladze@epam.com>
2023-02-05 21:00:21 +0400

    test

--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 
 describe('App', () => {
-    it('renders headline', () => {
+    it('renders headline2', () => {
         render(<App />);
         const heading = screen.getByText('Vite + React');
         expect(heading).toBeInTheDocument();
